f593a5ed11f3d7d94c6d5fa1dd3c3440d9e0cd5a
CLARENCE TAN WEIJUN 2200649H <2200649H@student.tp.edu.sg>
2024-01-28 20:29:08 +0800

    KIMM!!!!! I FIXED IT!

--- Transport_Booking/Client/Static/Endpoints.cs
+++ Transport_Booking/Client/Static/Endpoints.cs
@@ -8,7 +8,7 @@
         public static readonly string FeedbacksEndpoint = $"{Prefix}/feedbacks";
         public static readonly string StaffsEndpoint = $"{Prefix}/staffs";
         public static readonly string CustomersEndpoint = $"{Prefix}/customers";
-        public static readonly string PaymentsEndpoint = $"{Prefix}payments";
+        public static readonly string PaymentsEndpoint = $"{Prefix}/payments";
         public static readonly string TransportBookingsEndpoint = $"{Prefix}/transportbookings";
     }
 }
