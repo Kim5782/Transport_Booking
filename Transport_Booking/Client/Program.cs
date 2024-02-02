9c0638538437ce33cda6e746a5fd0cebd4d12d61
CLARENCE TAN WEIJUN 2200649H <2200649H@student.tp.edu.sg>
2024-02-02 19:06:45 +0800

    my part done

--- Transport_Booking/Client/Program.cs
+++ Transport_Booking/Client/Program.cs
@@ -2,17 +2,22 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Transport_Booking.Client;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+using Transport_Booking.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("Transport_Booking.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
+builder.Services.AddHttpClient("Transport_Booking.ServerAPI", (sp,client) => {client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+    client.EnableIntercept(sp);
+})
+.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Transport_Booking.ServerAPI"));
 
+builder.Services.AddHttpClientInterceptor();
 builder.Services.AddApiAuthorization();
+builder.Services.AddScoped<HttpInterceptorService>();
 
 await builder.Build().RunAsync();
